00eca9ce5da480863904151c3aa9de68e7180cbd
fakegermano <danielgtravieso@gmail.com>
2014-05-14 20:39:44 -0300

    parte b de fato

---
+++ ListaCliente.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ListaCliente {
+
+	private ArrayList<Cliente> lista;
+
+	public ListaCliente() {
+		/* TODO: inicialize as variaveis locais. */
+	}
+
+	public void add(Cliente cliente) {
+		/* TODO: adicione a lista o cliente. */
+	}
+
+	public ArrayList<Cliente> search(int opt) {
+		ArrayList<Cliente> myList = new ArrayList<Cliente>();
+
+		Pattern pattern;
+		Matcher matcher;
+
+		/* TODO: Desenvolva a busca para cada opcao utilizando regex. */
+		/* Utilize as funções Pattern.compile("regex"),
+		 * pattern.matcher("String") e matcher.lookinAt(). */
+
+		return myList;
+	}
+}
