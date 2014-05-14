4a8fdd2a8f345867f29fe6a59b92f9005a8ceee6
fakegermano <danielgtravieso@gmail.com>
2014-05-14 20:37:11 -0300

    parte b

--- Main.java
+++ Main.java
@@ -1,66 +1,83 @@
-
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	boolean valido = true;
+	public static void main(String[] args) {
+		ListaCliente lista = new ListaCliente();
 
-	/* Recebe entradas. */
-	System.out.println("Primeiro nome:");
-	String nome = scanner.nextLine();
-	System.out.println("Sobrenome:");
-	String sobrenome = scanner.nextLine();
-	System.out.println("Endereco:");
-	String endereco = scanner.nextLine();
-	System.out.println("Cidade:");
-	String cidade = scanner.nextLine();
-	System.out.println("Estado:");
-	String estado = scanner.nextLine();
-	System.out.println("CEP:");
-	String cep = scanner.nextLine();
-	System.out.println("Telefone:");
-	String telefone = scanner.nextLine();
+		Scanner scanner = new Scanner(System.in);
+		String comando;
 
-	/* Valida entrada. */
-	System.out.println("\nResultado da validacao:");
-	if (!ValidaEntrada.validaNome(nome)) {
-	    valido = false;
-	    System.out.println("Primeiro nome invalido");
-	}
-	if (!ValidaEntrada.validaSobrenome(sobrenome)) {
-	    valido = false;
-	    System.out.println("Sobrenome invalido");
-	}
-	if (!ValidaEntrada.validaEndereco(endereco)) {
-	    valido = false;
-	    System.out.println("Endereco invalido");
-	}
-	if (!ValidaEntrada.validaCidade(cidade)) {
-	    valido = false;
-	    System.out.println("Cidade invalida");
-	}
-	if (!ValidaEntrada.validaEstado(estado)) {
-	    valido = false;
-	    System.out.println("Estado invalido");
-	}
-	if (!ValidaEntrada.validaCEP(cep)) {
-	    valido = false;
-	    System.out.println("CEP invalido");
-	}
-	if (!ValidaEntrada.validaTelefone(telefone)) {
-	    valido = false;
-	    System.out.println("Telefone invalido");
-	}
+		do {
+			comando = scanner.nextLine();
 
-	if (valido) {
-	    System.out.println("Entradas validas.");
+			if (comando.equals("add")) {
+
+				/* Recebe entradas. */
+				System.out.println("Primeiro nome:");
+				String nome = scanner.nextLine();
+				System.out.println("Sobrenome:");
+				String sobrenome = scanner.nextLine();
+				System.out.println("Endereco:");
+				String endereco = scanner.nextLine();
+				System.out.println("Cidade:");
+				String cidade = scanner.nextLine();
+				System.out.println("Estado:");
+				String estado = scanner.nextLine();
+				System.out.println("CEP:");
+				String cep = scanner.nextLine();
+				System.out.println("Telefone:");
+				String telefone = scanner.nextLine();
+
+				/* Valida entrada. */
+				if (ValidaEntrada.validaNome(nome)
+						&& ValidaEntrada.validaSobrenome(sobrenome)
+						&& ValidaEntrada.validaEndereco(endereco)
+						&& ValidaEntrada.validaCidade(cidade)
+						&& ValidaEntrada.validaEstado(estado)
+						&& ValidaEntrada.validaCEP(cep)
+						&& ValidaEntrada.validaTelefone(telefone)) {
+
+					/* Salva cliente na lista. */
+					Cliente cliente = new Cliente();
+					cliente.setNome(nome);
+					cliente.setSobrenome(sobrenome);
+					cliente.setEndereco(endereco);
+					cliente.setCidade(cidade);
+					cliente.setEstado(estado);
+					cliente.setCep(cep);
+					cliente.setTelefone(telefone);
+
+					lista.add(cliente);
+					System.out.println("Adicionado!");
+
+				} else {
+					System.out.println("Dados invalidos!");
+				}
+
+			} else if (comando.equals("search")) {
+
+				ArrayList<Cliente> mySearch = lista.search(scanner.nextInt());
+				System.out.println();
+
+				if (mySearch.isEmpty()) {
+					System.out.println("Nao ha clientes a serem exibidos.");
+				} else {
+					for (Cliente cliente : mySearch) {
+						System.out.println(cliente.toString());
+					}
+				}
+
+			} else if (comando.equals("exit")) {
+
+				break;
+
+			}
+
+		} while (true);
+
+		scanner.close();
+
 	}
-
-	scanner.close();
-
-    }
-
 }
