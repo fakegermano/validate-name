55c74394bcb2d489ef72ee4b6f2f29a3c5661bf8
Daniel Travieso <danielgtravieso@gmail.com>
2014-05-17 01:13:58 -0300

    parteb

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,99 @@
+
+
 public class Cliente {
+	private String nome;
+	private String sobrenome;
+	private String endereco;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String telefone;
 	
+	public String toString() {
+		return this.nome + " " + this.sobrenome;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the sobrenome
+	 */
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	/**
+	 * @param sobrenome the sobrenome to set
+	 */
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	/**
+	 * @return the endereco
+	 */
+	public String getEndereco() {
+		return endereco;
+	}
+	/**
+	 * @param endereco the endereco to set
+	 */
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	/**
+	 * @return the cidade
+	 */
+	public String getCidade() {
+		return cidade;
+	}
+	/**
+	 * @param cidade the cidade to set
+	 */
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	/**
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	/**
+	 * @return the cep
+	 */
+	public String getCep() {
+		return cep;
+	}
+	/**
+	 * @param cep the cep to set
+	 */
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	/**
+	 * @return the telefone
+	 */
+	public String getTelefone() {
+		return telefone;
+	}
+	/**
+	 * @param telefone the telefone to set
+	 */
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 }
--- ListaCliente.java
+++ ListaCliente.java
@@ -1,3 +1,5 @@
+
+
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,11 +9,11 @@ public class ListaCliente {
 	private ArrayList<Cliente> lista;
 
 	public ListaCliente() {
-		/* TODO: inicialize as variaveis locais. */
+		this.lista = new ArrayList<Cliente>();
 	}
 
 	public void add(Cliente cliente) {
-		/* TODO: adicione a lista o cliente. */
+		lista.add(cliente);
 	}
 
 	public ArrayList<Cliente> search(int opt) {
@@ -19,11 +21,89 @@ public class ListaCliente {
 
 		Pattern pattern;
 		Matcher matcher;
+		for (Cliente cliente : this.lista) {
+			switch (opt) {
+			case 0:
+				pattern = Pattern.compile("\\d{2} [2357]{4}\\-[2357].*");
 
-		/* TODO: Desenvolva a busca para cada opcao utilizando regex. */
-		/* Utilize as funções Pattern.compile("regex"),
-		 * pattern.matcher("String") e matcher.lookinAt(). */
+				matcher = pattern.matcher(cliente.getTelefone());
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
 
+				break;
+			case 1:
+				pattern = Pattern.compile(".*([a-z])([a-z])[a-z]\\2\\1.*");
+
+				matcher = pattern.matcher(cliente.getNome().toLowerCase());
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
+
+				break;
+			case 2:
+				pattern = Pattern.compile(".*([^aeiou][aeiou]){3}.*");
+
+				matcher = pattern.matcher(cliente.getSobrenome().toLowerCase());
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
+
+				break;
+			case 3:
+				pattern = Pattern.compile("\\A[\\S]+\\s[\\S]+\\s[\\S]+\\Z");
+
+				matcher = pattern.matcher(cliente.getCidade());
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
+
+				break;
+			case 4:
+				pattern = Pattern.compile(".*\\-.*");
+
+				matcher = pattern.matcher(cliente.getSobrenome());
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
+
+				break;
+			case 5:
+				pattern = Pattern.compile("([a-zA-Z])[^$]*\\$\\1.*");
+
+				matcher = pattern.matcher(cliente.getCidade().concat("$")
+						.concat(cliente.getEstado()));
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
+
+				break;
+			case 6:
+				String regex1 = "(\\d)(\\d) ((?=.*\\1).*(?=.*\\2).*)";
+				String regex2 = "(\\d)\\1 .*(?=\\1.*(?=\\1)).*";
+				char array[] = cliente.getTelefone().toCharArray();
+				if (array[0] == array[1]) {
+					pattern = Pattern.compile(regex2);
+				} else {
+					pattern = Pattern.compile(regex1);
+				}
+				matcher = pattern.matcher(cliente.getTelefone().substring(0, 9));
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
+
+				break;
+			case 7:
+				pattern = Pattern.compile("[^$]*([aeiouAEIOU])[^$]*([aeiouAEIOU])[^$]*"
+						+ "\\$(?=.*\\1(?=.*\\2)).*");
+				matcher = pattern.matcher(cliente.getNome().concat("$")
+						.concat(cliente.getSobrenome()));
+				if (matcher.lookingAt()) {
+					myList.add(cliente);
+				}
+				break;
+			}
+		}
 		return myList;
 	}
 }
